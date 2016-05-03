4e1c92230bc5449e1dde10b8c09ca292e7e5fe45
sallop <shiogai1987@yahoo.co.jp>
2016-05-03 20:06:16 +0900

    write sort table

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from 'angular2/core';
 import { MONKEYS } from './mock-monkeys';
 import { OrderBy } from './orderBy';
+import * as Sort from './sort';
+
 //import { AgGridNg2 } from 'ag-grid-ng2/main';
 
 @Component({
@@ -9,21 +11,20 @@ import { OrderBy } from './orderBy';
   <h1>member of project mayhem</h1>
   <table>
     <thead>
-      <td (click)="hello('id');">id</td>
-      <td (click)="hello('team');">team</td>
-      <td (click)="hello('name');">name</td>
-      <td (click)="hello('christian name');">christian name</td>
-      <td (click)="hello('birthday');">birthday</td>
+      <td (click)="clicked('id');">id</td>
+      <td (click)="clicked('team');">team</td>
+      <td (click)="clicked('name');">name</td>
+      <td (click)="clicked('christian_name');">christian name</td>
+      <td (click)="clicked('birthday');">birthday</td>
     </thead>
-   <!--tr *ngFor="let monkey of monkeys | orderBy : '-team' : sign"-->
-   <tr *ngFor="let monkey of monkeys | orderBy : sign">
+   <tr *ngFor="let monkey of monkeys">
       <td>{{monkey.id}}</td>
       <td>{{monkey.team}}</td>
       <td>{{monkey.name}}</td>
       <td>{{monkey.christian_name}}</td>
       <td>{{monkey.birthday}}</td>
     </tr>
-    <button (click)="clicked();">Click</button>
+    <button (click)="clicked('id');">Click</button>
   </table>
   `,
   pipes: [OrderBy]
@@ -32,13 +33,25 @@ import { OrderBy } from './orderBy';
 export class AppComponent {
   public monkeys = MONKEYS;
   public sign = '+';
-  public hello( message ){
-    console.log( message );
-  }
-  public clicked(){
-    this.sign = this.sign == '+' ? '-' : '+';
+  public sort_type = Sort.compare_id;
+  public sort_type_table = {
+    "id": Sort.compare_id,
+    "team": Sort.compare_team,
+    "name": Sort.compare_name,
+    "christian_name": Sort.compare_christian_name,
+    "birthday": Sort.compare_birthday
+  };
+
+  public clicked(sort_type){
+    this.sort_type = this.sort_type_table[sort_type];
+    if (this.sign == '+'){
+      this.sign = this.sign == '+' ? '-' : '+';
+      this.monkeys.sort(this.sort_type);
+    } else {
+      this.sign = this.sign == '+' ? '-' : '+';
+      this.monkeys.sort(this.sort_type).reverse();
+    }
     console.log("Hello World!");
     console.log(this.sign);
   }
-
 }
---
+++ src/sort.ts
@@ -0,0 +1,56 @@
+import {Monkey} from './monkey';
+import {MONKEYS} from './mock-monkeys';
+
+console.log("This is sort.ts");
+
+//var unordered = MONKEYS;
+var unordered = [
+  new Monkey(1, 4, "Marla", "Maria", "1984/03/11"),
+  new Monkey(2, 8, "lobart", "bitch tits", "1984/07/11"),
+  new Monkey(3, 1, "Edward Norton", "Tyler Durden", "1984/09/11"),
+  new Monkey(4, 3, "Officer", "dildo", "1984/09/11"),
+  new Monkey(5, 2, "Bob", "bitch tits", "1984/07/11"),
+];
+
+//const ordered = [];
+
+//console.log(unordered);
+//console.log(JSON.stringify(unordered));
+
+//constructor(id: number, team: number, name: string, christian_name: string, birthday: string){
+export function compare_id(a, b){
+  if (a.id < b.id) return -1;
+  else if (a.id > b.id) return 1;
+  else return 0;
+}
+
+export function compare_team(a, b){
+  if (a.team < b.team) return -1;
+  else if (a.team > b.team) return 1;
+  else return 0;
+}
+
+export function compare_name(a, b){
+  if (a.name < b.name) return -1;
+  else if (a.name > b.name) return 1;
+  else return 0;
+}
+
+export function compare_christian_name(a, b){
+  if (a.christian_name < b.christian_name) return -1;
+  else if (a.christian_name > b.christian_name) return 1;
+  else return 0;
+}
+
+export function compare_birthday(a, b){
+  if (a.birthday < b.birthday) return -1;
+  else if (a.birthday > b.birthday) return 1;
+  else return 0;
+}
+//unordered.sort(compare);
+//ordered = unordered.sort(compare_team).reverse();
+//console.log(JSON.stringify(ordered));
+//console.log("unordered");
+//console.log(unordered);
+//console.log("ordered");
+//console.log(ordered);
